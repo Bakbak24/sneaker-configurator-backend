adc63f1c1dfc62d9f6fc21c6688cc7af4b2513d2
Bakbak24 <zakabak3@gmail.com>
2024-11-01 17:30:57 +0100

    feat: add auth controller to users route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const authController = require("../controllers/auth");
 
 router.post("/signup", function (req, res, next) {
   res.send("test");
