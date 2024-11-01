9b73388789f24c429fd4d7b39642293e0dfa7af5
Bakbak24 <zakabak3@gmail.com>
2024-11-01 17:09:39 +0100

    feat: refactor order model into separate file and update orderController to use it

--- controllers/api/v1/orderController.js
+++ controllers/api/v1/orderController.js
@@ -1,43 +1,4 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const orderSchema = new Schema({
-  customerName: {
-    type: String,
-    required: true,
-  },
-  customerEmail: {
-    type: String,
-    required: true,
-  },
-  shoeSize: {
-    type: Number,
-    required: true,
-  },
-  laceColor: {
-    type: String,
-    required: true,
-  },
-  soleColor: {
-    type: String,
-    required: true,
-  },
-  extraOptions: {
-    charms: { type: String, default: null },
-    patterns: { type: String, default: null },
-    material: { type: String, default: null },
-    customText: { type: String, default: null },
-  },
-  status: {
-    type: String,
-    enum: ["in productie", "verzonden", "afgehandeld", "geannuleerd"],
-    default: "in productie",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
-const Order = mongoose.model("Order", orderSchema);
+const Order = require("../../../models/Order");
 
 const index = async (req, res) => {
   try {
