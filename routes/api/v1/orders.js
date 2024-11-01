e840af8cc652445b9faa04fc2680c934e74561a4
Bakbak24 <zakabak3@gmail.com>
2024-11-01 20:24:46 +0100

    feat: add admin authorization middleware for update and delete orders

--- routes/api/v1/orders.js
+++ routes/api/v1/orders.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const orderController = require("../../../controllers/api/v1/orderController");
+const passport = require("../../../passport/passport");
+const isAdmin = require("../../../middlewares/auth");
 
 router.get("/", orderController.index);
 router.get("/:id", orderController.getOrderById);
 router.post("/", orderController.create);
-router.put("/:id", orderController.update);
-router.delete("/:id", orderController.remove);
+router.put("/:id", passport.authenticate("jwt", { session: false }), isAdmin, orderController.update);
+router.delete("/:id", passport.authenticate("jwt", { session: false }), isAdmin, orderController.remove);
 
 module.exports = router;
