e1b2f643b47f358c631946eb20581e3bcac09f6a
Bakbak24 <zakabak3@gmail.com>
2024-12-07 21:21:47 +0100

    feat: remove JWT authentication middleware from orders API route

--- app.js
+++ app.js
@@ -28,11 +28,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use(
-  "/api/v1/orders",
-  passport.authenticate("jwt", { session: false }),
-  apiOrdersRouter
-);
+app.use("/api/v1/orders", apiOrdersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
