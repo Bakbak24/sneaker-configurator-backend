74a9e7c0047fe28596009c07349e7b906b1ffc7c
Bakbak24 <zakabak3@gmail.com>
2024-12-08 19:19:11 +0100

    feat: add middleware to set secure test cookie with HttpOnly and SameSite attributes

--- app.js
+++ app.js
@@ -30,6 +30,15 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/api/v1/orders", apiOrdersRouter);
 
+app.use((req, res, next) => {
+  res.cookie("testCookie", "testValue", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+  });
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -37,7 +46,6 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
