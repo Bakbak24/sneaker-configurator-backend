d09a0279697d32bf11bd47c86af441022f791275
Bakbak24 <zakabak3@gmail.com>
2024-12-08 19:12:53 +0100

    feat: update CORS middleware to allow all origins and improve error handling

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ const usersRouter = require("./routes/users");
 const apiOrdersRouter = require("./routes/api/v1/orders");
 const passport = require("./passport/passport");
 const config = require("config");
-const mongoose = require("mongoose");
 
+const mongoose = require("mongoose");
 mongoose.connect(process.env.DB_CONN || config.get("Database.conn"));
 
 const app = express();
 
+// view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
@@ -22,29 +23,25 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use(
-  cors({
-    origin: "https://sneaker-configurator-three.vercel.app",
-    credentials: true,
-  })
-);
-
 app.use(express.static(path.join(__dirname, "public")));
 
-// Routes
+app.use(cors());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/api/v1/orders", apiOrdersRouter);
 
+// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
+// error handler
 app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
+  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
